299679c2d1fa9c2af93b8f8ffbe08c829f376385
kaizoku49 <tapanparek20@gmail.com>
2019-09-17 21:47:52 +0530

    put on my bio

---
+++ loke-arouet/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    var description = new Typed('#description', {
+        strings: ['Tech Enthusiast^2500', 'Go Programmer^2500', 'Flutter Developer^2500', 'Memer^2500'],
+        shuffle: true,
+        loop: true,
+        typeSpeed: 100,
+        backSpeed: 100,
+        cursorChar: '_',
+    })
+})
